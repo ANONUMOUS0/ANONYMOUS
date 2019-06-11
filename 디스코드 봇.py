d141b9cd6c1e9a7f997bfa8d11af165271043670
ANONUMOUS0 <51699857+ANONUMOUS0@users.noreply.github.com>
2019-06-11 18:22:55 +0900

    Add files via upload

---
+++ 디스코드 봇.py
@@ -0,0 +1,35 @@
+import discord
+import openpyxl
+
+client = discord.Client()
+
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("구매문의 익명#4735")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("안녕"):
+        await message.channel.send("반가워")
+    if message.content.startswith("뭐해"):
+        await message.channel.send("익명샵에서 핵사는중")
+    if message.content.startswith("놀자"):
+        await message.channel.send("구매하면 놀아줄게")
+
+    if message.content.startswith("/사진"):
+        pic = message.content.split(" ")[1]
+        await message.channel.send(file=discord.File(pic))
+
+
+
+
+
+client.run("NTg1ODE1Njg2MzE2NTU2MzE4.XPe9vQ.rSfRk1vU6nAaFA-ORpOQSgs2IaU")
+
